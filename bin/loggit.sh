f9dd18bb27bf390c6630eceec01bbe8be3d6dbe7
Vineeth Raj <contact.twn@openmailbox.org>
2014-10-26 11:52:54 +0530

    loggit: make sure we write to a file
    using zzz_last.sh, and ctrl+c'ing the script made a mess of things,
    overwritten logs as a result, so write to a file which is anyways
    the last thanks to the file sorting algorithm.

--- bin/loggit.sh
+++ bin/loggit.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 mkdir -p ~/logs;
-FILE=~/logs/log${1}_`date +%d%b_%H-%M`.txt
-FILE_S=~/logs/log${1}_`date +%d%b_%H-%M-%S`.txt
+FILE=~/logs/log${1}_`date +%d%b_%H-%M-%S`.txt
 
-if [ ! -f $FILE ]; then
-	adb logcat | tee ~/logs/zzz_last.txt;
-	mv ~/logs/zzz_last.txt ${FILE}
-else
-	adb logcat | tee ~/logs/zzz_last.txt;
-	mv ~/logs/zzz_last.txt ${FILE_S}
-fi
+adb logcat | tee ${FILE}
