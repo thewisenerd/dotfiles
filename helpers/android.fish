2bd73620a99b0a2154ec2d89d871d8b81ccb598c
thewisenerd <thewisenerd@protonmail.com>
2017-01-22 11:42:35 +0530

    helpers/android: better path handling

--- helpers/android.fish
+++ helpers/android.fish
@@ -6,6 +6,3 @@ set -x PATH {$EXTENTS_PATH}/helpers/android $PATH
 
 # ferrari
 set -x PATH {$EXTENTS_PATH}/helpers/android/ferrari $PATH
-
-# tc
-set -x PATH {$WORKS_PATH}/android/toolchains/aarch64-linux-android-4.9/bin $PATH
