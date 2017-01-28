46aaab846a5cb525d8816ac3d43fe93df8b09f7b
thewisenerd <thewisenerd@protonmail.com>
2017-01-28 16:47:39 +0530

    xmonad: add viewShift

--- .xmonad.symlink/xmonad.hs
+++ .xmonad.symlink/xmonad.hs
@@ -11,25 +11,46 @@ import XMonad.Util.EZConfig
 
 import XMonad.Actions.NoBorders
 
+import Control.Monad (liftM2)
+import qualified XMonad.StackSet as W
+
+import XMonad.Actions.WindowBringer
+
 myLayoutHook = (tabbed shrinkText def) ||| layoutHook defaultConfig
 
-myManageHook = composeAll [
-    resource =? "VLC" --> doFloat
-  ]
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
 
-newManageHook = myManageHook <+> manageHook defaultConfig
+myShiftHooks = composeAll
+	[
+		className =? "Terminator"		--> viewShift	(myWorkspaces !! 0)
+	,	className =? "Geany"			--> viewShift	(myWorkspaces !! 1)
+	,	className =? "Firefox"			--> viewShift	(myWorkspaces !! 2)
+	,	className =? "Chromium"			--> viewShift	(myWorkspaces !! 2)
+	,	className =? "Spacefm"			--> viewShift	(myWorkspaces !! 3)
+	,	className =? "Vlc"			--> viewShift	(myWorkspaces !! 6)
+	,	className =? "Tixati"			--> doShift	(myWorkspaces !! 8)
+	]
+	where viewShift = doF . liftM2 (.) W.greedyView W.shift
+
+myFloatHooks = composeAll
+	[
+		resource =? "Vlc"			--> doFloat
+	]
 
 main = xmonad $ defaultConfig
-  {
-    normalBorderColor = "#332d29",
-    focusedBorderColor = "#817267",
-    borderWidth = 4,
-    terminal = "terminator",
-    modMask  = mod1Mask,
-    manageHook = newManageHook,
-    layoutHook = myLayoutHook
+	{
+		normalBorderColor = "#332d29"
+	,	focusedBorderColor = "#817267"
+	,	borderWidth = 4
+	,	terminal = "terminator"
+	,	modMask  = mod1Mask
+	,	layoutHook = myLayoutHook
+	,	workspaces = myWorkspaces
+	,	manageHook = myShiftHooks <+> myFloatHooks <+> manageHook defaultConfig
   }
-  `additionalKeys`
-  [
-    ((mod1Mask .|. shiftMask , xK_b), withFocused toggleBorder)
-  ]
+	`additionalKeys`
+	[
+		((mod1Mask .|. shiftMask , xK_b), withFocused toggleBorder)
+	,	((mod1Mask, xK_g ), gotoMenu)
+	,	((mod1Mask, xK_b ), bringMenu)
+	]
