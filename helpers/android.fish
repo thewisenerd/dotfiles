df753bd8a01425abbfd1a5806342490c61b8d1a0
thewisenerd <thewisenerd@protonmail.com>
2017-01-28 16:38:42 +0530

    android: add simg2img

--- helpers/android.fish
+++ helpers/android.fish
@@ -9,3 +9,6 @@ set -x PATH {$EXTENTS_PATH}/helpers/android/ferrari $PATH
 
 # sdat2img
 set -x PATH {$WORKS_PATH}/android/sdat2img $PATH
+
+# simg2img
+set -x PATH {$WORKS_PATH}/android/android-simg2img $PATH
