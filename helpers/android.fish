3adb31f78033b678a592340e21d38373afbddd02
thewisenerd <thewisenerd@protonmail.com>
2017-01-28 16:37:44 +0530

    android: add sdat2img

--- helpers/android.fish
+++ helpers/android.fish
@@ -6,3 +6,6 @@ set -x PATH {$EXTENTS_PATH}/helpers/android $PATH
 
 # ferrari
 set -x PATH {$EXTENTS_PATH}/helpers/android/ferrari $PATH
+
+# sdat2img
+set -x PATH {$WORKS_PATH}/android/sdat2img $PATH
