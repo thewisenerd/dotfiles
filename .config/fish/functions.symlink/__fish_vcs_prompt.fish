901e21787b6d2c35a9f75e49602acecef73a7f50
thewisenerd <thewisenerd@protonmail.com>
2017-06-20 00:01:18 +0530

    fish: functions cleanup
    will add back later if necessary

--- .config/fish/functions.symlink/__fish_vcs_prompt.fish
+++
@@ -1,3 +0,0 @@
-function __fish_vcs_prompt
-	__fish_git_prompt
-end
--- .config/fish/functions.symlink/opibenv.fish
+++
@@ -1,7 +0,0 @@
-# function opibenv
-# 	set -gx ARCH "arm"
-# 	set -gx CROSS_COMPILE "arm-opi-linux-gnueabihf-"
-# 
-# 	mkdir -p "$HOME/works/opipc/linux-modules"
-# 	set -gx INSTALL_MOD_PATH "$HOME/works/opipc/linux-modules"
-# end
