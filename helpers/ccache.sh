d1b3557a3abb433224e2ea968680cfebdee8c47c
thewisenerd <thewisenerd@protonmail.com>
2017-06-20 00:08:16 +0530

    move ccache configuration to helpers

--- helpers/ccache.sh
+++ helpers/ccache.sh
@@ -1,2 +1,10 @@
 # ccache
 export PATH="/usr/lib/ccache/bin":$PATH
+
+export CCACHE_DIR=${HOME}/works/.ccache;
+export CCACHE=1;
+export USE_CCACHE=1;
+export CCACHE_NOCOMPRESS=1;
+export CCACHE_SLOPPINESS=time_macros,include_file_mtime,file_macro;
+export CCACHE_COMPILERCHECK=content;
+export CCACHE_MAXSIZE="40.0G"
