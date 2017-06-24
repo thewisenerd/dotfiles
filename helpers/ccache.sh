37c40e09c8e7b4b1ff5c5fb3c1cf5d4fa610a324
thewisenerd <thewisenerd@protonmail.com>
2017-06-25 01:04:15 +0530

    helpers; fix ccache envvar

--- helpers/ccache.sh
+++ helpers/ccache.sh
@@ -2,7 +2,7 @@
 export PATH="/usr/lib/ccache/bin":$PATH
 
 export CCACHE_DIR=${HOME}/works/.ccache;
-export CCACHE=1;
+export CCACHE="/usr/bin/ccache";
 export USE_CCACHE=1;
 export CCACHE_NOCOMPRESS=1;
 export CCACHE_SLOPPINESS=time_macros,include_file_mtime,file_macro;
