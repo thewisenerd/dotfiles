44aac613afa228dd73f6a321b7231a50a266eccf
thewisenerd <thewisenerd@protonmail.com>
2017-12-24 07:56:14 +0530

    npm: no sudo pls

---
+++ helpers/node.sh
@@ -0,0 +1,11 @@
+# thanks https://github.com/sindresorhus/guides/blob/master/npm-global-without-sudo.md
+
+NPM_PACKAGES="${HOME}/works/.node"
+
+mkdir -p "$NPM_PACKAGES"
+
+export PATH="$NPM_PACKAGES/bin:$PATH"
+
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
