ad657317a29e24d508771ff04fea0cf04e053d7a
thewisenerd <thewisenerd@protonmail.com>
2018-05-11 17:47:59 +0530

    helpers: bash: fix PS1

--- helpers/bash.sh
+++ helpers/bash.sh
@@ -1,2 +1,11 @@
 # ps1
-export PS1="bash \$( fish -c 'fish_prompt' )"
+
+RED="\[$(tput setaf 1)\]"
+GREEN="\[$(tput setaf 2)\]"
+RESET="\[$(tput sgr0)\]"
+
+if [ "$EUID" -eq 0 ]; then
+	export PS1="bash ${RED}$( fish -c 'prompt_pwd' )${RESET}# "
+else
+	export PS1="bash ${GREEN}$( fish -c 'prompt_pwd' )${RESET}> "
+fi
