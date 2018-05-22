54100a6e15c4e4247b53ead3c3c94ca78419e8e2
thewisenerd <thewisenerd@protonmail.com>
2018-05-22 17:12:28 +0530

    helpers: fix bash prompt not being updated

--- helpers/bash.sh
+++ helpers/bash.sh
@@ -5,7 +5,7 @@ GREEN="\[$(tput setaf 2)\]"
 RESET="\[$(tput sgr0)\]"
 
 if [ "$EUID" -eq 0 ]; then
-	export PS1="bash ${RED}$( fish -c 'prompt_pwd' )${RESET}# "
+	export PS1="bash ${RED}\$( fish -c 'prompt_pwd' )${RESET}# "
 else
-	export PS1="bash ${GREEN}$( fish -c 'prompt_pwd' )${RESET}> "
+	export PS1="bash ${GREEN}\$( fish -c 'prompt_pwd' )${RESET}> "
 fi
