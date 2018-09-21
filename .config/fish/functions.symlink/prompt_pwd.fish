3d80f31e2d05e587cebcdbf79f16f7a90f964510
thewisenerd <thewisenerd@protonmail.com>
2018-09-21 23:43:11 +0530

    prompt_pwd: flip logic

--- .config/fish/functions.symlink/prompt_pwd.fish
+++ .config/fish/functions.symlink/prompt_pwd.fish
@@ -13,14 +13,16 @@ function prompt_pwd --description 'Print the current working directory, shortene
 
 	# print $PWD outside $HOME
 	set realhome ~
-	if [ "z$PWD" != "z$realhome" ]
-		set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+	if [ "z$PWD" = "z$realhome" ]
+		return 0
+	end
 
-		if [ $fish_prompt_pwd_dir_length -eq 0 ]
-			echo $tmp
-		else
-			# Shorten to at most $fish_prompt_pwd_dir_length characters per directory
-			string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $tmp
-		end
-	end # $PWD != ~
+	set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+
+	if [ $fish_prompt_pwd_dir_length -eq 0 ]
+		echo $tmp
+	else
+		# Shorten to at most $fish_prompt_pwd_dir_length characters per directory
+		string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1/' $tmp
+	end
 end
