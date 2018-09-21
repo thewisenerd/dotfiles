669ed627b3aa84da7559f5c318ec27daf40adc14
thewisenerd <thewisenerd@protonmail.com>
2018-09-22 01:03:07 +0530

    prompt: add helper for zxpath

---
+++ helpers/zxpath.sh
@@ -0,0 +1,13 @@
+add_spath() {
+	[ -z "$ZXPATH" ] && {
+		export ZXPATH="$1"
+		export ZXKEYS="$2"
+	} || {
+		export ZXPATH="$ZXPATH:$1"
+		export ZXKEYS="$ZXKEYS:$2"
+	}
+}
+
+add_spath "$DOTFILES_PATH" "dots"
+
+unset -f add_spath
